42b482a6bbb7a697bb3f3d6171644d1425f9c865
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-11-01 16:48:39 -0600

    commit tree 8ac16c11bab863bad7c63daf677f521c4e6c6afe

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
 
+    attr_accessor :name, :breed
+    def initialize(name="Dog", breed="Dog")
+
+        @name = name
+        @breed = breed
+    end
+
+    # def name=(name)
+    #     @name = name
+    # end
+    # def name
+    #     @name
+    # end
+
 end
